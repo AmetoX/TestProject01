4c3baf36d3f1e6ee306182c27cc4996896cf706f
AmetoX <xametox.officiall@yahoo.com>
2022-06-10 18:32:01 +0300

    test01

---
+++ Test01/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace Test01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test01Stack();
+            //Test02BK();
+        }
+        static void Test01Stack()
+        {
+            Stack test01 = new Stack();//ultimul element introdus va fi primul in vector
+            test01.Push(2);
+            test01.Push(3);
+            test01.Push(4);
+            test01.Push(5);
+            test01.Push(6);
+            test01.Push(7);
+            test01.View();
+            //test01.Pop();
+            //test01.Pop2(); 
+            test01.Pop3(1);
+            test01.View();
+        }
+        static void Test02BK()//generare patrat perfect
+        {
+            int n = 9;
+            int[] s = new int[n];
+            bool[] b = new bool[n];
+            BK(0, n, s, b);
+        }
+        static void BK(int k, int n, int[] s, bool[] b)
+        {
+            if (k >= n)
+            {
+                int sl1 = s[0] + s[1] + s[2];
+                int sl2 = s[3] + s[4] + s[5];
+                int sl3 = s[6] + s[7] + s[8];
+
+                int sc1 = s[0] + s[3] + s[6];
+                int sc2 = s[1] + s[4] + s[7];
+                int sc3 = s[2] + s[5] + s[8];
+
+                int sd1 = s[0] + s[4] + s[8];
+                int sd2 = s[2] + s[4] + s[6];
+                if (sl1 == sl2 && sl1 == sl3 && sl1 == sc1 && sl1 == sc2 && sl1 == sc3 && sl1 == sd1 && sl1 == sd2)
+                {
+                    int l = 0;
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.Write(s[i] + " ");
+                        l++;
+                        if (l == 3)
+                        {
+                            Console.WriteLine();
+                            l = 0;
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (!b[i])
+                    {
+                        b[i] = true;
+                        s[k] = i + 1;
+                        BK(k + 1, n, s, b);
+                        b[i] = false;
+                    }
+                }
+            }
+        }
+        static void BubbleSort(int[] v)
+        {
+            int n = v.Length;
+            bool sw;
+            for (int i = 0; i < n - 1; i++)
+            {
+                sw = false;
+                for (int j = 0; j < n - 1; j++)
+                {
+                    if (v[j] > v[j + 1])
+                    {
+                        int temp = v[j];
+                        v[j] = v[j + 1];
+                        v[j + 1] = temp;
+                        sw = true;
+                    }
+                }
+                if (!sw)
+                    break;
+            }
+        }
+    }
+}
---
+++ Test01/Stack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01
+{
+    internal class Stack
+    {
+        int[] val;
+        public Stack() { val = new int[0]; }
+        public void Push(int v)
+        {
+            int[] t = new int[val.Length + 1];
+            for(int i = 0; i < val.Length; i++) { t[i+1]=val[i]; }
+            t[0] = v;
+            val = t;
+        }
+        public int Pop()//scot primul element introdus(ultimul element din vector) [queue]
+        {
+            int tor = val[val.Length-1];
+            int[] t = new int[val.Length - 1];
+            for(int i = 0; i < val.Length - 1; i++) { t[i] = val[i]; }
+            val = t;
+            return tor;
+        }
+        public int Pop2()//scot ultimul element introdus(primul element din vector) [stack]
+        {
+            int tor = val[0];
+            int[] t = new int[val.Length - 1];
+            for (int i = 0; i < val.Length - 1; i++) { t[i] = val[i+1]; }
+            val = t;
+            return tor;
+        }
+        public void Pop3(int v)//scot elemente de pe o pozitie anume
+        {
+            int[] t = new int[val.Length - 1];
+            for (int i = 0; i < val.Length-1; i++)
+            {
+                if (i >= v)
+                {
+                    int aux = val[i];
+                    val[i] = val[i + 1];
+                    val[i + 1] = aux;
+                    t[i] = val[i];
+                }
+                else
+                    t[i] = val[i];
+            }
+            val = t;
+        }
+        public void View()
+        {
+            for (int i = 0; i < val.Length; i++) { Console.Write(val[i]+" "); }
+            Console.WriteLine();
+        }
+    }
+}
